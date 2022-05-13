f24030943ccfddaeb3c215cb91c4ef585cd9f8e5
Varun <98093422+kVarunkk@users.noreply.github.com>
2022-05-13 11:32:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,3 +45,38 @@ document.addEventListener("keyup", function(event) {
       addItem();
   }
 });
+
+
+
+
+var box = document.getElementsByClass('list');
+  
+  /* listen to the touchMove event,
+  every time it fires, grab the location
+  of touch and assign it to box */
+  
+  for (let i = 0; i < box.length; i++) {
+    const element = box[i];
+    element.addEventListener('touchmove', function(e) {
+      // grab the location of touch
+      var touchLocation = e.targetTouches[0];
+      
+      // assign box new coordinates based on the touch.
+      element.style.left = touchLocation.pageX + 'px';
+      element.style.top = touchLocation.pageY + 'px';
+    })
+  }
+  
+  /* record the position of the touch
+  when released using touchend event.
+  This will be the drop position. */
+  
+  for (let i = 0; i < box.length; i++) {
+    const element = box[i];
+    element.addEventListener('touchend', function(e) {
+      // current box position.
+      var x = parseInt(element.style.left);
+      var y = parseInt(element.style.top);
+    })
+    
+  }
