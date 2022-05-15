6439ec971d2fb3397161d3d84cfd8484bbdadd39
Varun <98093422+kVarunkk@users.noreply.github.com>
2022-05-15 20:56:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,99 +1,111 @@
-const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+// select the todo-form
+const todoForm = document.querySelector('form');
+// select the input box
+const todoInput = document.querySelector('.type');
+// select the <ul> with class="todo-items"
+const todoItemsList = document.querySelector('.list-wrapper');
+// array which stores every todos
+let todos = [];
 
 
-var listContainer = document.querySelector('.list-wrapper');
-
-let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [] ;
-
-var newListInput = document.querySelector('.type');
-
-var form = document.querySelector('form');
-
-var cancelButtons =  document.getElementsByClassName("cancel");
+// add an eventListener on form, and listen for submit event
+todoForm.addEventListener('submit', function(event) {
+  // prevent the page from reloading when submitting the form
+  event.preventDefault();
+  addTodo(todoInput.value); // call addTodo function with input box current value
+});
 
 
-
-
-form.addEventListener('submit',e=>{
-  e.preventDefault();
-  const listName = newListInput.value;
-  if(listName == null || listName === '') return;
-  const list = createList(listName);
-  newListInput.value = null;
-  lists.push(list);
-  saveAndRender();
-})
-
-
-function createList(name){
-  return name;
-}
-
-function saveAndRender(){
-  save();
-  render();
-}
-
-function save(){
-  localStorage.setItem(LOCAL_STORAGE_LIST_KEY,JSON.stringify(lists));
-}
-
-
-function render(){
-  clearElement(listContainer);
-  lists.forEach((list,index) => {
-    const listElement = document.createElement('div');
-    listElement.classList.add('list');
-    listElement.classList.add('draggable');
-    listElement.setAttribute("draggable","true");
-    listElement.innerText = list;
-
-    var newButton = document.createElement('button');
-    newButton.classList.add('cancel');
-    var node2 = document.createTextNode('X');
-    newButton.appendChild(node2);
-    listElement.appendChild(newButton);
-
-    listContainer.appendChild(listElement);
-
-    remove();
-
-    newButton.addEventListener('click',()=>{
-      deleteItem(index);
-    })
-    
-
-  })
-}
-
-function clearElement(element){
-  while(element.firstChild){
-    element.removeChild(element.firstChild);
+// function to add todo
+function addTodo(item) {
+  // if item is not empty
+  if (item !== '') {
+    // make a todo object, which has id, name, and completed properties
+    const todo = {
+      id: Date.now(),
+      name: item,
+      completed: false
+    };
+    // then add it to todos array
+    todos.push(todo);
+    addToLocalStorage(todos); // then store it in localStorage
+    // finally clear the input box value
+    todoInput.value = '';
   }
 }
 
 
-function remove(){
+// function to render given todos to screen
+function renderTodos(todos) {
+  // clear everything inside <ul> with class=todo-items
+  todoItemsList.innerHTML = '';
+  // run through each item inside todos
+  todos.forEach(function(item) {
+    
+    const li = document.createElement('div');
+    
+    li.setAttribute('class', 'item');
+    
+    li.setAttribute('data-key', item.id);
 
-  for (let i = 0; i < cancelButtons.length; i++) {
-        cancelButtons[i].onclick = function () {
-          var div = this.parentElement;
-          div.style.display = "none";
-          // localStorage.removeItem(LOCAL_STORAGE_LIST_KEY) 
-        }
-      }
+    li.classList.add('draggable');
+    li.setAttribute("draggable","true");
+    
 
-     
+    
+
+    li.innerHTML = `${item.name} <button class="cancel">X</button>`;
+
+
+    // finally add the <li> to the list wrapper
+    todoItemsList.append(li);
+  });}
+  
+
+
+  // function to add todos to local storage
+function addToLocalStorage(todos) {
+  // convert the array to string then store it.
+  localStorage.setItem('todos', JSON.stringify(todos));
+  // render them to screen
+  renderTodos(todos);
 }
 
-function deleteItem(index) {
-  let todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY))
-  todos.splice(index, 1) 
-  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(todos)) 
-} 
 
-// todos.forEach((element, index) =>{ })
+// function helps to get everything from local storage
+function getFromLocalStorage() {
+  const reference = localStorage.getItem('todos');
+  // if reference exists
+  if (reference) {
+    // converts back to array and store it in todos array
+    todos = JSON.parse(reference);
+    renderTodos(todos);
+  }
+}
 
 
+// deletes a todo from todos array, then updates localstorage and renders updated list to screen
+function deleteTodo(id) {
+  // filters out the <li> with the id and updates the todos array
+  todos = todos.filter(function(item) {
+    // use != not !==, because here types are different. One is number and other is string
+    return item.id != id;
+  });
+  // update the localStorage
+  addToLocalStorage(todos);
+}
 
-render();
+
+// initially get everything from localStorage
+getFromLocalStorage();
+
+
+// after that addEventListener to list wrapper. Because we need to listen for click event in all delete-button 
+todoItemsList.addEventListener('click', function(event) {
+  
+  // check if that is a delete-button
+  if (event.target.classList.contains('cancel')) {
+    // get id from data-key attribute's value of parent <li> where the delete-button is present
+    deleteTodo(event.target.parentElement.getAttribute('data-key'));
+  }
+});
